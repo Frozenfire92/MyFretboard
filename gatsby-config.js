4b9d1a9f543b4d570a2a78da9f5bf96ab31b3617
Joel Kuntz <joel.kuntz.92@gmail.com>
2019-02-03 15:49:26 -0400

    #8 Setup PWA files

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,27 @@ module.exports = {
   plugins: [
     'gatsby-transformer-json',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/data/`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'My Fretboard',
+        short_name: 'MyFretboard',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+    'gatsby-plugin-offline'
   ],
   siteMetadata: {
     title: 'My Fretboard',
